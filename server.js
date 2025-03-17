959a89aa6eda48e64fb7d0e3de0999a91be31778
Stepan Rad <stepanrad@MacBook-Air-Stepan.local>
2025-03-17 13:27:15 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/user');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/user', userRoute);
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Connecting to bd and running...')
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
